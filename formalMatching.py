0acebb7f84a391092a27e0c9c6727de6f3b3d32b
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-06-02 16:46:04 -0500

    formal matches final

---
+++ formalMatching.py
@@ -0,0 +1,201 @@
+prefs = [
+  {"sponsor": "lindsay tracy", "choices": ["ify ifediba","emma luten"]},
+  {"sponsor": "sydney chiu", "choices": []},
+  {"sponsor": "sophia rotman", "choices": []},
+  {"sponsor": "christina wang", "choices": ["catherine bi","kristen koyanagi"]},
+  {"sponsor": "betsy weiner", "choices": ["janet taylor"]},
+  {"sponsor": "alyssa tarrant", "choices": ["marisa chao","brooke fraser"]},
+  {"sponsor": "rachel lowen", "choices": ["maddy mchugh","lindsay tracy"]},
+  {"sponsor": "emily miller", "choices": ["haley baker","alyssa tarrant"]},
+  {"sponsor": "jessica wen", "choices": []},
+  {"sponsor": "alison lee", "choices": ["marisa ilag","angela yim"]},
+  {"sponsor": "natascha lamprecht", "choices": ["ruth chen","lora zuo"]},
+  {"sponsor": "leigh brosof", "choices": ["lauren wachenfeld"]},
+  {"sponsor": "neeti shenoy", "choices": ["jessica wen","wenny zhang"]},
+  {"sponsor": "kiran kuttickat", "choices": ["wenny zhang","neeti shenoy"]},
+  {"sponsor": "lauren wachenfeld", "choices": ["leigh brosof"]},
+  {"sponsor": "chandler elliott-fehle", "choices": ["delaney earley","emily coco"]},
+  {"sponsor": "caroline moore", "choices": ["jessica wen","georgia redd"]},
+  {"sponsor": "madison felman", "choices": ["denise monti","holly ravazzolo"]},
+  {"sponsor": "denise monti", "choices": ["meredith klashman","madison felman"]},
+  {"sponsor": "grace mcdonnell", "choices": ["lucy nachmanoff"]},
+  {"sponsor": "felicia ellsworth", "choices": ["susannah price"]},
+  {"sponsor": "aysswarya manoharan", "choices": ["maddy mchugh"]},
+  {"sponsor": "wenny zhang", "choices": ["divya joshi"]},
+  {"sponsor": "julianne borrelli", "choices": ["nikki metzger","georgia redd"]},
+  {"sponsor": "namrata vakkalagadda", "choices": ["hannah woolf","georgia redd"]},
+  {"sponsor": "sara martino", "choices": ["alicia garten","madelaine azar"]},
+  {"sponsor": "nicole jordan", "choices": ["ruth chen"]},
+  {"sponsor": "victoria rabuse", "choices": ["molly shallow","emily coco"]},
+  {"sponsor": "michelle zhang", "choices": ["gracie liu","kathryn jan"]},
+  {"sponsor": "sarah hands", "choices": ["aysswarya manoharan","lora zuo"]},
+  {"sponsor": "jackie schechter", "choices": ["natascha lamprecht","ruth chen"]},
+  {"sponsor": "ryan brandt", "choices": ["maddy mchugh","madi kasoff"]},
+  {"sponsor": "anita mahadevan", "choices": []},
+  {"sponsor": "kaci karlsson", "choices": ["ruth chen"]},
+  {"sponsor": "alix halloran", "choices": ["lauren wachenfeld","mara getz"]},
+  {"sponsor": "katharine jaruzelski", "choices": ["caroline gutbezahl"]},
+  {"sponsor": "elyse lopez", "choices": ["samantha hindes","marisa chao"]},
+  {"sponsor": "ruth chen", "choices": ["natascha lamprecht","kaci karlsson"]},
+  {"sponsor": "marisa ilag", "choices": ["maddy mchugh","alison lee"]},
+  {"sponsor": "lucy nachmanoff", "choices": ["grace mcdonnell"]},
+  {"sponsor": "isabelle kornblau", "choices": ["lauren","felicia ellsworth"]},
+  {"sponsor": "molly shalllow", "choices": ["victoria rabuse","emily coco"]},
+  {"sponsor": "kathryn jan", "choices": ["michelle zhang","sherry huang"]},
+  {"sponsor": "lauren rogge", "choices": ["melanie cohen","corey salzer"]},
+  {"sponsor": "georgia redd", "choices": ["julianne borelli","namrata vakkalagadda"]},
+  {"sponsor": "holly baldacci", "choices": ["brooke fraser"]},
+  {"sponsor": "molly charney", "choices": ["hayley herzog","ally dworetsky"]},
+  {"sponsor": "emily graupmann", "choices": ["sophia rotman","onyou kang"]},
+  {"sponsor": "adriana chepote", "choices": ["emma turnbull","lucy gao"]},
+  {"sponsor": "hanita epstein", "choices": ["ify ifediba"]},
+  {"sponsor": "natasha suterwala", "choices": ["aliza astrow","alison bijarano"]},
+  {"sponsor": "marisa chao", "choices": ["samantha hindes"]},
+  {"sponsor": "yuemin li", "choices": ["mariel jung"]},
+  {"sponsor": "katrina joseph", "choices": ["emma turnbull","kathryn achuck"]},
+  {"sponsor": "delaney earley", "choices": ["chandler elliott-fehle","emily coco"]},
+  {"sponsor": "hayley herzog", "choices": ["molly charney"]},
+  {"sponsor": "brooke reiner", "choices": ["rachel lowen","madi kasoff"]},
+  {"sponsor": "kristen koyanagi", "choices": ["kathryn achuck","christina wang"]},
+  {"sponsor": "madison stoecker", "choices": ["tara gildersleeve","maddie lee"]},
+  {"sponsor": "susannah price", "choices": ["felicia ellsworth"]},
+  {"sponsor": "shawnee gao", "choices": ["meredith klashman","denise monti"]},
+  {"sponsor": "emma turnbull", "choices": ["katrina joseph","adriana chepote"]},
+  {"sponsor": "elaine cole", "choices": ["lindsey seigel"]},
+  {"sponsor": "sherry huang", "choices": ["kathryn jaruzelski","kristen koyanagi"]},
+  {"sponsor": "onyou kang", "choices": ["gracie liu","ayanna harrison"]},
+  {"sponsor": "maureen flaherman", "choices": ["sarah spellman","ceili ecker"]},
+  {"sponsor": "aliza astrow", "choices": ["natasha suterwala"]},
+  {"sponsor": "isabella solaro", "choices": ["clara modlin"]},
+  {"sponsor": "alison bidjarano", "choices": ["morissa schochet"]},
+  {"sponsor": "remy servis", "choices": ["caroline gutbezahl","clara modlin"]},
+  {"sponsor": "meredith klashman", "choices": ["denise monti","lauren wachenfeld"]},
+  {"sponsor": "maddy mchugh", "choices": ["rachel lowen"]},
+  {"sponsor": "madi kasoff", "choices": ["maddy mchugh","lindsay tracy"]},
+  {"sponsor": "alison schreiber", "choices": ["remy servis"]},
+  {"sponsor": "madelaine azar", "choices": ["alicia garten","sara martino"]},
+  {"sponsor": "katie fleischer", "choices": ["ally dworetsky"]},
+  {"sponsor": "clara modlin", "choices": ["emily coco","maddy mchugh"]},
+  {"sponsor": "emily coco", "choices": ["victoria rabuse","clara modlin"]},
+  {"sponsor": "sam myers", "choices": ["sarah spellman","ayanna harrison"]},
+  {"sponsor": "kathryn achuck", "choices": ["kristen koyanagi","catherine bi"]},
+  {"sponsor": "tara gildersleeve", "choices": ["madison stoecker"]},
+  {"sponsor": "varsha kumar", "choices": ["natasha suterwala","kayla king"]},
+  {"sponsor": "kayla moller", "choices": ["denise monti","meredith klashman"]},
+  {"sponsor": "kisha bwenge", "choices": ["remy servis","melanie cohen"]},
+  {"sponsor": "ally dworetsky", "choices": ["katie fleischer"]},
+  {"sponsor": "sara schatz", "choices": ["kayla king","sydney chiu"]},
+  {"sponsor": "sarah spellman", "choices": ["sam myers"]},
+  {"sponsor": "haley baker", "choices": ["meredith klashman"]},
+  {"sponsor": "divya joshi", "choices": ["wenny zhang"]},
+  {"sponsor": "brooke ivey", "choices": ["mara evans"]},
+  {"sponsor": "alicia garten", "choices": ["madelaine azar"]},
+  {"sponsor": "kelly young", "choices": ["ki mckinley"]},
+  {"sponsor": "maddie lee", "choices": ["madison stoecker","tara gildersleeve"]},
+  {"sponsor": "megan foley", "choices": ["alicia garten","meredith klashman"]},
+  {"sponsor": "mara getz", "choices": ["ruth chen","kelly young"]},
+  {"sponsor": "emily mark", "choices": ["alicia garten","ryan brandt"]},
+  {"sponsor": "mariel jung", "choices": ["kathryn achuck"]},
+  {"sponsor": "karen shen", "choices": ["morissa schochet","madison wrobley"]},
+  {"sponsor": "janet taylor", "choices": ["anna mckee","melanie cohen"]},
+  {"sponsor": "jenn smith", "choices": []},
+  {"sponsor": "courtney brien", "choices": ["ruth chen","anita mahadevan"]},
+  {"sponsor": "brooke fraser", "choices": ["holly baldacci","morissa schochet"]},
+  {"sponsor": "ayanna harrison", "choices": ["gracie liu","onyou kang"]},
+  {"sponsor": "lien tran", "choices": ["sydney chiu","sara"]},
+  {"sponsor": "lucy gao", "choices": ["emma turnbull","adriana chepote"]},
+  {"sponsor": "stacey harimoto", "choices": ["lien tran","maddie lee"]},
+  {"sponsor": "morissa schochet", "choices": ["alison bidjarano","natasha suterwala"]},
+  {"sponsor": "lora zuo", "choices": ["helen kwon","natascha lamprecht"]},
+  {"sponsor": "anna mckee", "choices": ["janet taylor"]},
+  {"sponsor": "nikki metzger", "choices": ["rachel kahn"]},
+  {"sponsor": "ruyan zhang", "choices": ["diana hsiao","lien tran"]},
+  {"sponsor": "gabby bloom", "choices": ["maddy mchugh","adriana chepote"]}
+]
+
+from pprint import pprint
+
+possibles = []
+nixed_names = []
+no_currs = []
+matches = []
+cant_match = []
+
+for p in prefs:
+    possibles.append(p['sponsor'])
+
+def valid_choice(c):
+    return c in possibles
+
+for p in prefs:
+    p['choices'] = filter(valid_choice, p['choices'])
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names and len(p['choices']) > 0:
+        first_preference = p['choices'][0]
+        for preffed in prefs:
+            if preffed['sponsor'] == first_preference and preffed['sponsor'] not in nixed_names:
+                if len(preffed['choices']) > 0:
+                    if preffed['choices'][0] == p['sponsor']:
+                        matches.append([p['sponsor'], preffed['sponsor']])
+                        nixed_names.append(p['sponsor'])
+                        nixed_names.append(preffed['sponsor'])
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names and len(p['choices']) == 1:
+        if p['choices'][0] in nixed_names:
+            p['choices'] = []
+    elif p['sponsor'] not in nixed_names and len(p['choices']) == 2:
+        if p['choices'][0] in nixed_names and p['choices'][1] in nixed_names:
+            p['choices'] = []
+        elif p['choices'][0] in nixed_names:
+            p['choices'] = [p['choices'][1]]
+        elif p['choices'][1] in nixed_names:
+            p['choices'] = [p['choices'][0]]
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names and len(p['choices']) > 0:
+        first_preference = p['choices'][0]
+        for preffed in prefs:
+            if preffed['sponsor'] == first_preference and preffed['sponsor'] not in nixed_names:
+                if len(preffed['choices']) > 0:
+                    if preffed['choices'][0] == p['sponsor']:
+                        matches.append([p['sponsor'], preffed['sponsor']])
+                        nixed_names.append(p['sponsor'])
+                        nixed_names.append(preffed['sponsor'])
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names and len(p['choices']) == 1:
+        if p['choices'][0] in nixed_names:
+            p['choices'] = []
+    elif p['sponsor'] not in nixed_names and len(p['choices']) == 2:
+        if p['choices'][0] in nixed_names and p['choices'][1] in nixed_names:
+            p['choices'] = []
+        elif p['choices'][0] in nixed_names:
+            p['choices'] = [p['choices'][1]]
+        elif p['choices'][1] in nixed_names:
+            p['choices'] = [p['choices'][0]]
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names:
+        if len(p['choices']) == 2:
+            if p['choices'][0] not in nixed_names and p['choices'][0] in possibles:
+                matches.append([p['sponsor'], p['choices'][0]])
+                nixed_names.append(p['sponsor'])
+                nixed_names.append(p['choices'][0])
+            elif p['choices'][1] not in nixed_names and p['choices'][1] in possibles:
+                matches.append([p['sponsor'], p['choices'][1]])
+                nixed_names.append(p['sponsor'])
+                nixed_names.append(p['choices'][1])
+
+        elif len(p['choices']) == 1:
+            if p['choices'][0] not in nixed_names and p['choices'][0] in possibles:
+                matches.append([p['sponsor'], p['choices'][0]])
+                nixed_names.append(p['sponsor'])
+                nixed_names.append(p['choices'][0])
+
+for p in prefs:
+    if p['sponsor'] not in nixed_names:
+        cant_match.append(p['sponsor'])
+
+import pdb; pdb.set_trace()
